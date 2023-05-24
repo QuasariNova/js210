93995d4157962fe50239d4f00fdd718816d688d6
Christopher Perkins <Christopher.Perkins@null.net>
2023-05-24 10:33:57 -0400

    L2A3: Further Explored.

--- lesson3/assignment3.js
+++ lesson3/assignment3.js
@@ -2,11 +2,13 @@
 // the odd numbers from 1 to the passed in number (inclusive). All numbers
 // should be logged on separate lines.
 
+/* Original
 function logOddNumbers(target) {
   for(let i = 1; i <= target; i += 2) {
     console.log(i);
   }
 }
+*/
 
 logOddNumbers(19);
 
@@ -21,3 +23,15 @@ logOddNumbers(19);
 // 15
 // 17
 // 19
+
+// Further exploration:
+// Instead of a conditional that checks for odd numbers, you can write a
+// conditional that checks for even numbers, but skips them with the continue
+// statement. How would you write your code?
+
+function logOddNumbers(target) {
+  for(let i = 1; i <= target; ++i) {
+    if (i % 2 == 0) continue;
+    console.log(i);
+  }
+}
