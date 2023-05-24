b3581f320c31237750510fde1d9b41f2ffbcb591
Christopher Perkins <Christopher.Perkins@null.net>
2023-05-24 10:43:58 -0400

    L2A4: Further Explored

--- lesson3/assignment4.js
+++ lesson3/assignment4.js
@@ -2,7 +2,7 @@
 // multiples of either 3 or 5. If the number is divisible by both 3 and 5,
 // append an "!" to the number.
 
-multiplesOfThreeAndFive();
+// multiplesOfThreeAndFive();
 
 // output on console
 // '3'
@@ -14,6 +14,7 @@ multiplesOfThreeAndFive();
 // '15!'
 // … remainder of output omitted for brevity
 
+/* Original:
 function multiplesOfThreeAndFive() {
   for(let i = 1; i <= 100; ++i) {
     if (i % 3 && i % 5) continue;
@@ -21,3 +22,19 @@ function multiplesOfThreeAndFive() {
     console.log(out);
   }
 }
+*/
+
+// Further Exploration
+// For additional practice, how would you change your function so it takes, as
+// arguments, the range of numbers it should check?
+
+function multiplesOfThreeAndFive(min, max) {
+  for(let i = min; i <= max; ++i) {
+    if (i % 3 && i % 5) continue;
+
+    out = i % 15 ? String(i) : `${i}!`;
+    console.log(out);
+  }
+}
+
+multiplesOfThreeAndFive(1, 20);
