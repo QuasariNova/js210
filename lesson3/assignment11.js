05ea2897de7c1d226646f8338fe8cc2396ff25a4
Christopher Perkins <Christopher.Perkins@null.net>
2023-05-24 12:50:24 -0400

    L3A11: Further Explored.

--- lesson3/assignment11.js
+++ lesson3/assignment11.js
@@ -5,6 +5,7 @@
 // gcd(15, 10);  // 5
 // gcd(9, 2);    // 1
 
+/* Original
 function gcd(a, b) {
   let high = a > b ? a : b;
   let low = b > a ? b : a;
@@ -12,4 +13,26 @@ function gcd(a, b) {
   let remainder = high % low;
   if (!remainder) return low;
   return gcd(low, remainder);
+}
+*/
+
+// Further Exploration:
+// Can you expand it to accept an array of two or more numbers instead?
+
+function gcd(numbers) {
+  let current = numbers[0];
+
+  for(let value of numbers.slice(1)) {
+    let high = current > value ? current : value;
+    let low = value < current ? value : current;
+
+    while (low) {
+      let remainder = high % low;
+      high = low;
+      low = remainder;
+    }
+
+    current = high;
+  }
+  return current;
 }
