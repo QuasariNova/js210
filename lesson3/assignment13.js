412425f1497f08059990dc2a3f92f660c80d0398
Christopher Perkins <Christopher.Perkins@null.net>
2023-05-25 10:13:24 -0400

    L3A13: Further Explored.

--- lesson3/assignment13.js
+++ lesson3/assignment13.js
@@ -13,6 +13,8 @@ generatePattern(7);
 // 123456*
 // 1234567
 
+/*
+// Original
 function generatePattern(nStars) {
   const STAR = '*';
   let numberString = '';
@@ -20,5 +22,28 @@ function generatePattern(nStars) {
   for(let i = nStars - 1; i >= 0; --i) {
     numberString += (nStars - i);
     console.log(numberString + STAR.repeat(i));
+  }
+}
+*/
+
+// Have you tried supplying generatePattern with a number greater than 9? What
+// did you observe? Can you fix the current implementation so that it still
+// renders as a rectangle? Try it yourself before scrolling down.
+
+generatePattern(20);
+
+function generatePattern(nStars) {
+  const STAR = '*';
+  let numberString = '';
+  let width = 0;
+
+  for (let i = 1; i <= nStars; ++i) {
+    width += String(i).length;
+  }
+
+  for(let i = nStars - 1; i >= 0; --i) {
+    let numbers = nStars - i;
+    numberString += numbers;
+    console.log(numberString + STAR.repeat(width - numberString.length));
   }
 }
