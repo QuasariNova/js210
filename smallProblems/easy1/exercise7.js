a7a8a1c1f5ba60ec802ecf95b03045674acc91c0
Christopher Perkins <Christopher.Perkins@null.net>
2023-05-26 16:18:58 -0400

    Small Problems: Easy I 7 futher explored.

--- smallProblems/easy1/exercise7.js
+++ smallProblems/easy1/exercise7.js
@@ -23,6 +23,45 @@ isLeapYear(1);         // false
 isLeapYear(100);       // false
 isLeapYear(400);       // true
 
+/*
+// original
 function isLeapYear(year) {
   return year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)
 }
+*/
+
+// Further Exploration
+// The order in which you perform tests for a leap year calculation is
+// important. For what years will isLeapYear fail if you rewrite it as shown
+// below?
+
+// function isLeapYear(year) {
+//   if (year % 100 === 0) {
+//     return false;
+//   } else if (year % 400 === 0) {
+//     return true;
+//   } else {
+//     return year % 4 === 0;
+//   }
+// }
+
+// = This will fail on years divisible by 400, since that is included in years
+//   divisible by 100.
+
+// Can you rewrite isLeapYear to perform its tests in the opposite order of the
+// above solution? That is, test whether the year is divisible by 4 first,
+// then, if necessary, test whether it is divisible by 100, and finally, if
+// necessary, test whether it is divisible by 400. Is this solution simpler or
+// more complex than the original solution?
+
+function isLeapYear(year) {
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400) return false;
+    }
+    return true;
+  }
+  return false;
+}
+
+// This is more complex.
