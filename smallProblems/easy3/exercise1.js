6e30dccce6b7aab09299f295b472c7ece22fb3be
Christopher Perkins <Christopher.Perkins@null.net>
2023-05-30 10:59:42 -0400

    Small Problems; Easy 3 1 further explored.

--- smallProblems/easy3/exercise1.js
+++ smallProblems/easy3/exercise1.js
@@ -12,3 +12,22 @@ function randomBetween(min, max) {
 const age = randomBetween(20, 200);
 
 console.log(`Teddy is ${age} years old!`);
+
+// Further Exploration
+// The randomBetween function used the Math.floor() method. Would it make a
+// difference if the Math.round() method was used instead?
+
+// Math.round() would give an uneven distribution for the highest and lowest
+// integers. It would also not need 1 to be added as it would include the max
+// value.
+
+// Also, how can we make the function more robust? What if the user
+// inadvertently gave the inputs in reverse order (i.e., the value passed to
+// min was greater than max)?
+
+/*
+function randomBetween(min, max) {
+  if (max < min) [min, max] = [max, min]; // swap if they are swapped
+  return  Math.floor(Math.random() * (max - min + 1)) + min;
+}
+*/
