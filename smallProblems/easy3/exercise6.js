19a8f8386b011c79e3324c61c723763900e1334c
Christopher Perkins <Christopher.Perkins@null.net>
2023-05-31 10:13:25 -0400

    Small Problems: Easy III 6 further explored.

--- smallProblems/easy3/exercise6.js
+++ smallProblems/easy3/exercise6.js
@@ -17,3 +17,17 @@ function isPalindrome(str) {
 function isPalindromicNumber(number) {
   return isPalindrome(String(number));
 }
+
+// Suppose the number argument begins with one or more 0s. Will the solution
+// still work? Why or why not? Is there any way to address this?
+
+// This would not work. JavaScript will truncate the excess 0s. If you want
+// to infer that there could be an equal number of 0's on the left as the right
+// you could remove the rightmost 0s before checking if its a palindrome:
+
+/*
+function isPalindromicNumber(number) {
+  number = String(number).replace(/0+$/, '');
+  return isPalindrome(number);
+}
+*/
