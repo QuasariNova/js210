9422d1ffedd85d9389bb7fa676b2842490dcb83d
Christopher Perkins <Christopher.Perkins@null.net>
2023-06-01 16:51:19 -0400

    Small Problems: Easy IV 1 further explored.

--- smallProblems/easy4/exercise1.js
+++ smallProblems/easy4/exercise1.js
@@ -6,6 +6,7 @@
 // seconds in a minute.
 
 const DMS_SUBUNIT_CONVERSION = 60;
+const DEGREE_LIMIT = 360;
 const SECONDS_IN_DEGREES = 3600;
 
 // Examples:
@@ -16,6 +17,10 @@ dms(254.6);        // 254°35'59"
 dms(93.034773);    // 93°02'05"
 dms(0);            // 0°00'00"
 dms(360);          // 360°00'00" or 0°00'00"
+dms(-1);           // 359°00'00"
+dms(400);          // 40°00'00"
+dms(-40);          // 320°00'00"
+dms(-420);         // 300°00'00"
 
 // Note: your results may differ slightly depending on how you round values,
 // but should generally be within a second or two of the results shown.
@@ -28,11 +33,31 @@ function divMod(value, divide) {
   return [Math.floor(value / divide), Math.floor(value % divide)];
 }
 
+/*
+// original
 function dms(angle) {
   const totalSeconds = (angle % 360) * SECONDS_IN_DEGREES;
   let degrees;
   let minutes;
   let seconds;
+
+  [minutes, seconds] = divMod(totalSeconds, DMS_SUBUNIT_CONVERSION);
+  [degrees, minutes] = divMod(minutes, DMS_SUBUNIT_CONVERSION);
+
+  console.log(`${degrees}°${padZeroes(minutes)}'${padZeroes(seconds)}"`);
+}
+*/
+
+// Further exploration
+// The current solution implementation only works with positive numbers in the
+// range of 0 to 360 (inclusive). Can you refactor it so that it works with any
+// positive or negative number?
+
+function dms(angle) {
+  angle = DEGREE_LIMIT + (angle % DEGREE_LIMIT);
+
+  const totalSeconds = (angle % DEGREE_LIMIT) * SECONDS_IN_DEGREES;
+  let degrees, minutes, seconds;
 
   [minutes, seconds] = divMod(totalSeconds, DMS_SUBUNIT_CONVERSION);
   [degrees, minutes] = divMod(minutes, DMS_SUBUNIT_CONVERSION);
