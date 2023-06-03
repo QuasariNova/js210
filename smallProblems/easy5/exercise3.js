99c152f409fc39bbff48b687cdda08d6c047f330
Christopher Perkins <Christopher.Perkins@null.net>
2023-06-03 07:51:16 -0400

    Small Problems: Easy V 3 solved.

---
+++ smallProblems/easy5/exercise3.js
@@ -0,0 +1,16 @@
+// Write a function that takes a positive integer as an argument and returns
+// that number with its digits reversed.
+
+// Examples:
+
+reverseNumber(12345);    // 54321
+reverseNumber(12213);    // 31221
+reverseNumber(456);      // 654
+reverseNumber(12000);    // 21 -- Note that zeros get dropped!
+reverseNumber(1);        // 1
+
+function reverseNumber(number) {
+  let reversedString = [...String(number)].reverse().join('');
+
+  return parseInt(reversedString, 10);
+}
